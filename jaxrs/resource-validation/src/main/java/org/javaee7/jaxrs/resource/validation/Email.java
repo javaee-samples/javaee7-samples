da1aa6307158a2f5098f4916cc63fe28845b1e8e
Arun Gupta <arun.p.gupta@oracle.com>
2013-08-27 21:48:06 +0200

    Removing a redundant import

--- jaxrs/resource-validation/src/main/java/org/javaee7/jaxrs/resource/validation/Email.java
+++ jaxrs/resource-validation/src/main/java/org/javaee7/jaxrs/resource/validation/Email.java
@@ -47,7 +47,6 @@ import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
