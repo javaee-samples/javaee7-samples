cac8f3f1b5b66e2ae3b93abfe2aef2c5f2cad836
Arun Gupta <arun.p.gupta@oracle.com>
2013-08-27 23:39:51 +0200

    Overriding appropriate method

--- batch/chunk-checkpoint/src/main/java/org/javaee7/batch/chunk/checkpoint/MyItemReader.java
+++ batch/chunk-checkpoint/src/main/java/org/javaee7/batch/chunk/checkpoint/MyItemReader.java
@@ -39,6 +39,7 @@
  */
 package org.javaee7.batch.chunk.checkpoint;
 
+import java.io.Serializable;
 import java.util.StringTokenizer;
 import javax.batch.api.chunk.AbstractItemReader;
 import javax.inject.Named;
@@ -49,10 +50,11 @@ import javax.inject.Named;
 @Named
 public class MyItemReader extends AbstractItemReader {
     
-    private final StringTokenizer tokens;
+    private StringTokenizer tokens;
     static int COUNT = 0;
-    
-    public MyItemReader() {
+
+    @Override
+    public void open(Serializable checkpoint) throws Exception {
         tokens = new StringTokenizer("1,2,3,4,5,6,7,8,9,10", ",");
     }
     
