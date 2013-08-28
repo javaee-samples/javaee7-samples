d7d31dcb75f985081d3b6928574041f59c6768e5
Arun Gupta <arun.p.gupta@oracle.com>
2013-08-29 00:47:39 +0300

    Removing fields that cannot be injected here

--- websocket/injection/src/main/java/org/javaee7/websocket/injection/MyEndpointWithCDI.java
+++ websocket/injection/src/main/java/org/javaee7/websocket/injection/MyEndpointWithCDI.java
@@ -40,25 +40,21 @@
 package org.javaee7.websocket.injection;
 
 import javax.inject.Inject;
-import javax.servlet.ServletContext;
 import javax.websocket.OnMessage;
 import javax.websocket.server.ServerEndpoint;
 
 /**
  * @author Arun Gupta
  */
-@ServerEndpoint(value="/websocket-cdi")
+@ServerEndpoint("/websocket-cdi")
 @Logging
 public class MyEndpointWithCDI {
     
     @Inject MyBean bean;
     
-    @Inject ServletContext servletContext;
-    
     @OnMessage
     public String sayHello(String name) {
         System.out.println(getClass().getName() + ".sayHello");
-        System.out.println("context path: " + servletContext.getContextPath());
-        return bean.sayHello(name);
+        return bean.sayHello(name + " (from CDI)");
     }
 }
