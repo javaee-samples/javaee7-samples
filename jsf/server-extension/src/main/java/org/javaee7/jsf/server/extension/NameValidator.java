b6d35eccb09d0ba6b55581dd179db54556306945
Arun Gupta <arun.p.gupta@oracle.com>
2013-08-28 22:55:52 +0300

    Throwing appropriate ValidatorException

--- jsf/server-extension/src/main/java/org/javaee7/jsf/server/extension/NameValidator.java
+++ jsf/server-extension/src/main/java/org/javaee7/jsf/server/extension/NameValidator.java
@@ -39,6 +39,7 @@
  */
 package org.javaee7.jsf.server.extension;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
@@ -54,6 +55,10 @@ public class NameValidator implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         System.out.println("Got: " + value);
+        if (((String)value).length() < 3)
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_WARN, 
+                    "Incorrect name length", 
+                    "Name length must >= 3, found only " + value));
     }
     
 }
