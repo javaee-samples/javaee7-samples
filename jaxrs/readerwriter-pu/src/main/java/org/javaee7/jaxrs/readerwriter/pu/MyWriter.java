23bfd943c1d946f0d41a0cc992f446e057504d9c
Arun Gupta <arun.p.gupta@oracle.com>
2013-09-10 13:54:14 -0700

    New sample to show PU injection in JAX-RS 2 Entity Provider - throwing NPE at this time

---
+++ jaxrs/readerwriter-pu/src/main/java/org/javaee7/jaxrs/readerwriter/pu/MyWriter.java
@@ -0,0 +1,87 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package org.javaee7.jaxrs.readerwriter.pu;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Arun Gupta
+ */
+@Provider
+@Produces(MyObject.MIME_TYPE)
+public class MyWriter implements MessageBodyWriter<MyObject> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type type1, Annotation[] antns, MediaType mt) {
+        return MyObject.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public long getSize(MyObject t, Class<?> type, Type type1, Annotation[] antns, MediaType mt) {
+        // As of JAX-RS 2.0, the method has been deprecated and the 
+        // value returned by the method is ignored by a JAX-RS runtime. 
+        // All MessageBodyWriter implementations are advised to return -1 from 
+        // the method.
+        
+        return -1;
+    }
+
+    @Override
+    public void writeTo(MyObject t, 
+                Class<?> type, 
+                Type type1, 
+                Annotation[] antns, 
+                MediaType mt, 
+                MultivaluedMap<String, Object> mm, 
+                OutputStream out) throws IOException, WebApplicationException {
+        ObjectOutputStream oos = new ObjectOutputStream(out);
+        oos.writeObject(t);
+    }
+}
