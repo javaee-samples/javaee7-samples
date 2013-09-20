48c9a3caecbcaf758df9602326bcfd02cfc386be
Arun Gupta <arun.p.gupta@oracle.com>
2013-09-20 09:12:51 -0700

    Adding @Produces

--- jaxrs/readerwriter-json/src/main/java/org/javaee7/jaxrs/readerwriter/json/MyResource.java
+++ jaxrs/readerwriter-json/src/main/java/org/javaee7/jaxrs/readerwriter/json/MyResource.java
@@ -42,6 +42,7 @@ package org.javaee7.jaxrs.readerwriter.json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -50,6 +51,7 @@ import javax.ws.rs.core.MediaType;
 @Path("endpoint")
 public class MyResource {
     @POST
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public MyObject echoObject(MyObject mo) {
         return mo;
