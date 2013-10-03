b6c5a686d6fef71458976769ef42abbafe49db5a
Arun Gupta <arun.p.gupta@oracle.com>
2013-10-03 14:14:11 -0700

    Fixing the package name

--- extra/nosql/mongo/src/main/java/org/javaee7/extra/nosql/mongo/PersonSessionBean.java
+++ extra/nosql/mongo/src/main/java/org/javaee7/extra/nosql/mongo/PersonSessionBean.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.javaee7.extra.mongo;
+package org.javaee7.extra.nosql.mongo;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
