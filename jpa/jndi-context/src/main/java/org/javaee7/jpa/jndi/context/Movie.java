b5beeee9238e50cf527654b345216c8e9426135a
Arun Gupta <arun.gupta@gmail.com>
2013-10-30 03:41:23 -0700

    Removing redundant imports

--- jpa/jndi-context/src/main/java/org/javaee7/jpa/jndi/context/Movie.java
+++ jpa/jndi-context/src/main/java/org/javaee7/jpa/jndi/context/Movie.java
@@ -41,14 +41,12 @@ package org.javaee7.jpa.jndi.context;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author Arun Gupta
