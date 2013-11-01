4cedd2cf7e45db4bde4c1e3b3b9da548e9b77202
Arun Gupta <arun.gupta@gmail.com>
2013-11-01 13:44:12 -0700

    Removing try/resources so that the stream is not closed and error handling is reported correctly

--- servlet/event-listeners/src/main/java/org/javaee7/servlet/event/listeners/TestServlet.java
+++ servlet/event-listeners/src/main/java/org/javaee7/servlet/event/listeners/TestServlet.java
@@ -54,9 +54,8 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -66,42 +65,40 @@ public class TestServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Event Listeners</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Event Listeners</h1>");
-            out.println("<h2>Setting, updating, and removing ServletContext Attributes</h2>");
-            request.getServletContext().setAttribute("attribute1", "attribute-value1");
-            request.getServletContext().setAttribute("attribute1", "attribute-updated-value1");
-            request.getServletContext().removeAttribute("attribute1");
-            out.println("done");
-            out.println("<h2>Setting, updating, and removing HttpSession Attributes</h2>");
-            request.getSession(true).setAttribute("attribute1", "attribute-value1");
-            request.getSession().setAttribute("attribute1", "attribute-updated-value1");
-            request.getSession().removeAttribute("attribute1");
-            out.println("done");
-            out.println("<h2>Setting, updating, and removing ServletRequest Attributes</h2>");
-            request.setAttribute("attribute1", "attribute-value1");
-            request.setAttribute("attribute1", "attribute-updated-value1");
-            request.removeAttribute("attribute1");
-            out.println("done");
-            out.println("<h2>Invalidating session</h2>");
-            request.getSession().invalidate();
-            out.println("done");
-            out.println("<br><br>Check output in server log");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet Event Listeners</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet Event Listeners</h1>");
+        out.println("<h2>Setting, updating, and removing ServletContext Attributes</h2>");
+        request.getServletContext().setAttribute("attribute1", "attribute-value1");
+        request.getServletContext().setAttribute("attribute1", "attribute-updated-value1");
+        request.getServletContext().removeAttribute("attribute1");
+        out.println("done");
+        out.println("<h2>Setting, updating, and removing HttpSession Attributes</h2>");
+        request.getSession(true).setAttribute("attribute1", "attribute-value1");
+        request.getSession().setAttribute("attribute1", "attribute-updated-value1");
+        request.getSession().removeAttribute("attribute1");
+        out.println("done");
+        out.println("<h2>Setting, updating, and removing ServletRequest Attributes</h2>");
+        request.setAttribute("attribute1", "attribute-value1");
+        request.setAttribute("attribute1", "attribute-updated-value1");
+        request.removeAttribute("attribute1");
+        out.println("done");
+        out.println("<h2>Invalidating session</h2>");
+        request.getSession().invalidate();
+        out.println("done");
+        out.println("<br><br>Check output in server log");
+        out.println("</body>");
+        out.println("</html>");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP
-     * <code>GET</code> method.
+     * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -115,8 +112,7 @@ public class TestServlet extends HttpServlet {
     }
 
     /**
-     * Handles the HTTP
-     * <code>POST</code> method.
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
