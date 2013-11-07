23a7d57f07e3c87653445da51a1ab3a0080f658e
Arun Gupta <arun.gupta@gmail.com>
2013-11-07 10:04:41 +0100

    Going back to 4 space indentation and removing Undertow-specific APIs (keep the code clean and appserver-agnostic)

--- websocket/binary/src/test/java/org/javaee7/websocket/binary/test/WebsocketBinaryEndpointTest.java
+++ websocket/binary/src/test/java/org/javaee7/websocket/binary/test/WebsocketBinaryEndpointTest.java
@@ -1,20 +1,20 @@
 /**
- * 
+ *
  */
 package org.javaee7.websocket.binary.test;
 
-import io.undertow.websockets.jsr.UndertowContainerProvider;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.websocket.ContainerProvider;
 
 import javax.websocket.DeploymentException;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 
-import junit.framework.Assert;
+import static junit.framework.Assert.assertNull;
 
 import org.javaee7.websocket.binary.MyEndpointByteArray;
 import org.javaee7.websocket.binary.MyEndpointByteBuffer;
@@ -29,85 +29,89 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * @author Nikos  Ballas
- * 
+ * @author Nikos Ballas
+ * @author Arun Gupta
  */
 @RunWith(Arquillian.class)
 public class WebsocketBinaryEndpointTest {
-	private static final String WEBAPP_SRC = "src/main/webapp";
 
-	/**
-	 * Arquillian specific method for creating a file which can be deployed
-	 * while executing the test.
-	 * 
-	 * @return a war file deployable in the jboss instance configuraed in arquillian.xml file.
-	 */
-	@Deployment(testable = false)
-	@TargetsContainer("wildfly-arquillian")
-	public static WebArchive createDeployment() {
-		WebArchive war = ShrinkWrap.create(WebArchive.class)
-				.addClass(MyEndpointByteBuffer.class)
-				.addClass(MyEndpointByteArray.class)
-				.addClass(MyEndpointInputStream.class)
-				.addClass(MyEndpointClient.class)
-				.addAsWebResource(new File(WEBAPP_SRC, "index.jsp"))
-				.addAsWebResource(new File(WEBAPP_SRC, "websocket.js"));
-		return war;
-	}
+    private static final String WEBAPP_SRC = "src/main/webapp";
 
-	/**
-	 * The basic test method for the class {@link MyEndpointByteBuffer}
-	 * 
-	 * @throws URISyntaxException
-	 * @throws DeploymentException
-	 * @throws IOException
-	 */
-	@Test
-	public void testEndpointByteBuffer() throws URISyntaxException,DeploymentException, IOException {
-		Session session = connectToServer("bytebuffer");
-		Assert.assertNull(session);
-	}
+    /**
+     * Arquillian specific method for creating a file which can be deployed
+     * while executing the test.
+     *
+     * @return a war file deployable in the jboss instance configuraed in
+     * arquillian.xml file.
+     */
+    @Deployment(testable = false)
+    @TargetsContainer("wildfly-arquillian")
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class)
+                .addClass(MyEndpointByteBuffer.class)
+                .addClass(MyEndpointByteArray.class)
+                .addClass(MyEndpointInputStream.class)
+                .addClass(MyEndpointClient.class)
+                .addAsWebResource(new File(WEBAPP_SRC, "index.jsp"))
+                .addAsWebResource(new File(WEBAPP_SRC, "websocket.js"));
+        return war;
+    }
 
-	/**
-	 * The basic test method for the class {@MyEndpointByteArray
-	 * }
-	 * 
-	 * @throws DeploymentException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	@Test
-	public void testEndpointByteArray() throws DeploymentException,IOException, URISyntaxException {
-		Session session = connectToServer("bytearray");
-		Assert.assertNull(session);
-	}
+    /**
+     * The basic test method for the class {@link MyEndpointByteBuffer}
+     *
+     * @throws URISyntaxException
+     * @throws DeploymentException
+     * @throws IOException
+     */
+    @Test
+    public void testEndpointByteBuffer() throws URISyntaxException, DeploymentException, IOException {
+        Session session = connectToServer("bytebuffer");
+        assertNull(session);
+    }
 
-	/**
-	 * The basic test method for the class {@MyEndpointInputStream
-	 * }
-	 * 
-	 * @throws DeploymentException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	@Test
-	public void testEndpointInputStream() throws DeploymentException,IOException, URISyntaxException {
-		Session session = connectToServer("inputstream");
-		Assert.assertNull(session);
-	}
+    /**
+     * The basic test method for the class {
+     *
+     * @MyEndpointByteArray }
+     *
+     * @throws DeploymentException
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testEndpointByteArray() throws DeploymentException, IOException, URISyntaxException {
+        Session session = connectToServer("bytearray");
+        assertNull(session);
+    }
 
-	/**
-	 * Method used to supply connection to the server by passing the naming of
-	 * the websocket endpoint
-	 * 
-	 * @param endpoint
-	 * @return
-	 * @throws DeploymentException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	public Session connectToServer(String endpoint) throws DeploymentException,	IOException, URISyntaxException {
-		WebSocketContainer wSocketContainer = UndertowContainerProvider.getWebSocketContainer();
-		return wSocketContainer.connectToServer(MyEndpointClient.class,	new URI("ws://localhost:8080/binary/" + endpoint));
-	}
+    /**
+     * The basic test method for the class {
+     *
+     * @MyEndpointInputStream }
+     *
+     * @throws DeploymentException
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    @Test
+    public void testEndpointInputStream() throws DeploymentException, IOException, URISyntaxException {
+        Session session = connectToServer("inputstream");
+        assertNull(session);
+    }
+
+    /**
+     * Method used to supply connection to the server by passing the naming of
+     * the websocket endpoint
+     *
+     * @param endpoint
+     * @return
+     * @throws DeploymentException
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public Session connectToServer(String endpoint) throws DeploymentException, IOException, URISyntaxException {
+        WebSocketContainer wSocketContainer = ContainerProvider.getWebSocketContainer();
+        return wSocketContainer.connectToServer(MyEndpointClient.class, new URI("ws://localhost:8080/binary/" + endpoint));
+    }
 }
