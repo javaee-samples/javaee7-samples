74a18b85abb585b221ef0bf94d960cf9f2e211d2
Arun Gupta <arun.gupta@gmail.com>
2013-11-09 23:25:13 +0200

    Adding @Asynchronous on method - works on both WildFly and GlassFish

--- ejb/async-ejb/src/main/java/org/javaee7/ejb/async/MyAsyncBean.java
+++ ejb/async-ejb/src/main/java/org/javaee7/ejb/async/MyAsyncBean.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 
 /**
@@ -12,6 +13,7 @@ import javax.ejb.Stateless;
 @Stateless
 public class MyAsyncBean {
 
+    @Asynchronous
     public Future<Integer> addNumbers(int n1, int n2) {
         try {
             // simulating a long running query
@@ -20,9 +22,5 @@ public class MyAsyncBean {
             Logger.getLogger(MyAsyncBean.class.getName()).log(Level.SEVERE, null, ex);
         }
         return new AsyncResult(n1 + n2);
-    }
-
-    public void doSomething(String what) {
-        System.out.println("Just did " + what);
     }
 }
