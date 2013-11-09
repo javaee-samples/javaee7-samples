6b614f97fae6b6e452ebe2b9246b3e130da30762
Arun Gupta <arun.gupta@gmail.com>
2013-11-09 15:12:30 +0200

    New sample to show @Index usage

---
+++ jpa/schema-gen-index/src/main/java/org/javaee7/jpa/index/Employee.java
@@ -0,0 +1,53 @@
+package org.javaee7.jpa.index;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * @author Arun Gupta
+ */
+@Entity
+@Table(name = "EMPLOYEE_SCHEMA_GEN_INDEX", 
+        indexes = @Index(columnList="NAME"))
+@NamedQueries({
+    @NamedQuery(name = "Employee.findAll", query = "SELECT e FROM Employee e")
+})
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    @Column(length=40)
+    private String name;
+    
+    public Employee() { }
+    
+    public Employee(String name) {
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
