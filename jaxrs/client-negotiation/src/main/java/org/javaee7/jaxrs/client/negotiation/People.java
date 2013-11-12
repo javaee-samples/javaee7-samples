36750847307baedbd01c97b1219f8511fa2df8b5
Aslak Knutsen <aslak@redhat.com>
2013-11-12 15:50:58 +0100

    Convert JAX-RS samples to Arquillian: Client Negotiation
    Issue: #78

---
+++ jaxrs/client-negotiation/src/main/java/org/javaee7/jaxrs/client/negotiation/People.java
@@ -0,0 +1,18 @@
+package org.javaee7.jaxrs.client.negotiation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement 
+public class People extends ArrayList<Person> {
+
+   private static final long serialVersionUID = 1L;
+   
+   @XmlElement(name = "person")
+   public List<Person> getPeople() {
+      return this;
+   }
+}
--- jaxrs/client-negotiation/src/test/java/org/javaee7/jaxrs/client/negotiation/MyResourceTest.java
+++ jaxrs/client-negotiation/src/test/java/org/javaee7/jaxrs/client/negotiation/MyResourceTest.java
@@ -7,34 +7,53 @@
 package org.javaee7.jaxrs.client.negotiation;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import org.custommonkey.xmlunit.XMLAssert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.xml.sax.SAXException;
 
 /**
  * @author Arun Gupta
  */
+@RunWith(Arquillian.class)
 public class MyResourceTest {
 
-    private static WebTarget target;
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+       return ShrinkWrap.create(WebArchive.class)
+             .addClasses(MyApplication.class, MyResource.class, People.class, Person.class);
+    }
     
-    @BeforeClass
-    public static void setUpClass() {
+    private WebTarget target;
+
+    @ArquillianResource
+    private URL base;
+
+    @Before
+    public void setUpClass() throws MalformedURLException {
         Client client = ClientBuilder.newClient();
-        target = client
-                .target("http://localhost:8080/client-negotiation/webresources/persons");
+        target = client.target(new URL(base, "webresources/persons").toExternalForm());
     }
 
     @Test
     public void testXML() throws SAXException, IOException {
         String xml = target.request("application/xml").get(String.class);
+        System.out.println(xml);
         XMLAssert.assertXMLEqual("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><people><person><age>1</age><name>Penny</name></person><person><age>2</age><name>Leonard</name></person><person><age>3</age><name>Sheldon</name></person></people>", xml);
     }
 
