4aa9d0539cc1f922a9914156b473292a20958aa0
Alexis Hassler <alexis@sewatech.org>
2013-11-13 00:50:02 +0100

    Added Arquillian test for the jta/transactional example

---
+++ jta/transactional/src/test/java/org/javaee7/jta/transactional/MyBeanTest.java
@@ -0,0 +1,57 @@
+package org.javaee7.jta.transactional;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.transaction.TransactionalException;
+
+/**
+ * @author Alexis Hassler
+ */
+@RunWith(Arquillian.class)
+public class MyBeanTest {
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(JavaArchive.class)
+                         .addClass(MyBean.class);
+    }
+
+    @Inject MyBean bean;
+
+
+    @Test
+    public void should_required_work() {
+         bean.required();
+    }
+
+    @Test
+    public void should_requiresNew_work() {
+        bean.requiresNew();
+    }
+
+    @Test(expected = TransactionalException.class)
+    public void should_mandatory_throw_exception() {
+        bean.mandatory();
+    }
+
+    @Test
+    public void should_supports_work() {
+        bean.supports();
+    }
+
+    @Test
+    public void should_notSupported_work() {
+        bean.notSupported();
+    }
+
+    @Test
+    public void should_never_work() {
+        bean.never();
+    }
+}
