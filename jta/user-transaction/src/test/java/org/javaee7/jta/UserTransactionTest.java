1059dca0ab78e1dee2c2a19c62c863ff932e35f4
Alexis Hassler <alexis@sewatech.org>
2013-11-12 13:21:18 +0100

    Added Arquillian test for the jta/user-transaction example

---
+++ jta/user-transaction/src/test/java/org/javaee7/jta/UserTransactionTest.java
@@ -0,0 +1,42 @@
+package org.javaee7.jta;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.transaction.*;
+
+@RunWith(Arquillian.class)
+public class UserTransactionTest {
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(JavaArchive.class)
+                         .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        // WF 8.0.0.Beta1 needs the beans.xml file. Shouldn't be the case with JavaEE 7
+    }
+
+    @Inject UserTransaction ut;
+
+    @Test
+    public void should_work_with_cdi() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException {
+        ut.begin();
+        ut.commit();
+    }
+
+    @Test
+    public void should_work_with_jndi() throws SystemException, NotSupportedException, HeuristicRollbackException, HeuristicMixedException, RollbackException, NamingException {
+        Context context = new InitialContext();
+        UserTransaction ut = (UserTransaction)context.lookup("java:comp/UserTransaction");
+        ut.begin();
+        ut.commit();
+    }
+}
