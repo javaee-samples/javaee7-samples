df50bf69000b5224ce522a9b79f2d5af8b0bdf1a
kubamarchwicki <kuba@marchwicki.pl>
2013-11-13 22:46:02 +0100

    Custom constraint validators for arun-gupta/javaee7-samples#85

---
+++ validation/custom-constraint/src/test/java/org/javaee7/validation/custom/constraint/CustomConstraintTest.java
@@ -0,0 +1,45 @@
+package org.javaee7.validation.custom.constraint;
+
+import javax.inject.Inject;
+import javax.validation.ConstraintViolationException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class CustomConstraintTest {
+
+	@Inject
+	MyBean bean;
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Deployment
+	public static Archive<?> deployment() {
+		return ShrinkWrap.create(JavaArchive.class).addClasses(MyBean.class)
+				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Test
+	public void saveZipCodeforUs() {
+		bean.saveZip("95051");
+	}
+	
+	@Test
+	public void saveZipCodeForIndia() {
+		thrown.equals(ConstraintViolationException.class);
+		thrown.expectMessage("javaee7.validation.custom.constraint.ZipCode");
+		thrown.expectMessage("saveZipIndia.arg0");
+		bean.saveZipIndia("95051");
+	}
+
+}
