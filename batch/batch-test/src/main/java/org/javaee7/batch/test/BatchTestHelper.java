48a5c9014066a8a324a4eecabba5302ab2e4c3a5
Roberto Cortez <radcortez@yahoo.com>
2013-11-18 09:58:59 +0000

    Refactored common methods used by batch tests to project batch-test. Added test for chuck-simple project.

---
+++ batch/batch-test/src/main/java/org/javaee7/batch/test/BatchTestHelper.java
@@ -0,0 +1,56 @@
+package org.javaee7.batch.test;
+
+import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.Metric;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Roberto Cortez
+ */
+public final class BatchTestHelper {
+    private static final int MAX_TRIES = 10;
+    private static final int THREAD_SLEEP = 100;
+
+    private BatchTestHelper() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * We need to keep the test running because JobOperator runs the batch job in an asynchronous way, so the
+     * JobExecution can be properly updated with the running job status.
+     *
+     * @param jobExecution
+     *         the JobExecution of the job that is being runned on JobOperator.
+     *
+     * @throws InterruptedException thrown by Thread.sleep.
+     */
+    public static void keepTestAlive(JobExecution jobExecution) throws InterruptedException {
+        int maxTries = 0;
+        while (!jobExecution.getBatchStatus().equals(BatchStatus.COMPLETED)) {
+            if (maxTries < MAX_TRIES) {
+                maxTries++;
+                Thread.sleep(THREAD_SLEEP);
+            } else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * Convert the Metric array contained in StepExecution to a key-value map for easy access to Metric parameters.
+     *
+     * @param metrics
+     *         a Metric array contained in StepExecution.
+     *
+     * @return a map view of the metrics array.
+     */
+    public static Map<Metric.MetricType, Long> getMetricsMap(Metric[] metrics) {
+        Map<Metric.MetricType, Long> metricsMap = new HashMap<>();
+        for (Metric metric : metrics) {
+            metricsMap.put(metric.getType(), metric.getValue());
+        }
+        return metricsMap;
+    }
+}
--- batch/chunk-simple/src/test/java/org/javaee7/batch/chunk/simple/ChunkSimpleTest.java
+++ batch/chunk-simple/src/test/java/org/javaee7/batch/chunk/simple/ChunkSimpleTest.java
@@ -1,4 +1,4 @@
-package org.javaee7.batch.batchlet.simple;
+package org.javaee7.batch.chunk.simple;
 
 import org.javaee7.batch.test.BatchTestHelper;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -15,31 +15,49 @@ import javax.batch.operations.JobOperator;
 import javax.batch.runtime.BatchRuntime;
 import javax.batch.runtime.BatchStatus;
 import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.Metric;
+import javax.batch.runtime.StepExecution;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
  * @author Roberto Cortez
  */
 @RunWith(Arquillian.class)
-public class MyBatchletTest {
+public class ChunkSimpleTest {
     @Deployment
     public static WebArchive createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class)
                                    .addClass(BatchTestHelper.class)
-                                   .addClass(MyBatchlet.class)
+                                   .addClass(MyInputRecord.class)
+                                   .addClass(MyItemProcessor.class)
+                                   .addClass(MyItemReader.class)
+                                   .addClass(MyItemWriter.class)
+                                   .addClass(MyOutputRecord.class)
                                    .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
-                                   .addAsResource("META-INF/batch-jobs/myJob.xml");
+                                   .addAsManifestResource("META-INF/batch-jobs/myJob.xml", "batch-jobs/myJob.xml");
         System.out.println(war.toString(true));
         return war;
     }
 
     @Test
-    public void testBatchletProcess() throws Exception {
+    public void testChunkSimple() throws Exception {
         JobOperator jobOperator = BatchRuntime.getJobOperator();
         Long executionId = jobOperator.start("myJob", new Properties());
         JobExecution jobExecution = jobOperator.getJobExecution(executionId);
 
         BatchTestHelper.keepTestAlive(jobExecution);
+
+        List<StepExecution> stepExecutions = jobOperator.getStepExecutions(executionId);
+        for (StepExecution stepExecution : stepExecutions) {
+            if (stepExecution.getStepName().equals("myStep")) {
+                Map<Metric.MetricType,Long> metricsMap = BatchTestHelper.getMetricsMap(stepExecution.getMetrics());
+                Assert.assertEquals((long) metricsMap.get(Metric.MetricType.READ_COUNT), 10L);
+                Assert.assertEquals((long) metricsMap.get(Metric.MetricType.WRITE_COUNT), 10L/2L);
+                Assert.assertEquals((long) metricsMap.get(Metric.MetricType.COMMIT_COUNT), 10L/3 + 10%3);
+            }
+        }
 
         Assert.assertEquals(jobExecution.getBatchStatus(), BatchStatus.COMPLETED);
     }
