2e1161f57fe1b1db1dd3f14c88eea5918b1332ea
Arun Gupta <arun.gupta@gmail.com>
2013-11-18 15:41:51 -0800

    Fixing the test to work with Arquillian

--- servlet/metadata-complete/src/main/java/org/javaee7/servlet/metadata/complete/TestServlet.java
+++ servlet/metadata-complete/src/main/java/org/javaee7/servlet/metadata/complete/TestServlet.java
@@ -72,7 +72,7 @@ public class TestServlet extends HttpServlet {
         out.println("<title>Servlet url-pattern in web.xml</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>c</h1>");
+        out.println("<h1>Servlet url-pattern in web.xml</h1>");
         out.println("</body>");
         out.println("</html>");
     }
--- servlet/metadata-complete/src/test/java/org/javaee7/servlet/metadata/complete/TestServletTest.java
+++ servlet/metadata-complete/src/test/java/org/javaee7/servlet/metadata/complete/TestServletTest.java
@@ -10,32 +10,39 @@ import com.meterware.httpunit.WebConversation;
 import com.meterware.httpunit.WebResponse;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.xml.sax.SAXException;
 
 /**
  * @author Arun Gupta
  */
-//@RunWith(Arquillian.class)
+@RunWith(Arquillian.class)
 public class TestServletTest {
-    
+
     private static final String WEBAPP_SRC = "src/main/webapp";
-    
+
+    @ArquillianResource
+    private URL base;
+
     /**
      * Arquillian specific method for creating a file which can be deployed
      * while executing the test.
      *
      * @return a war file
      */
-//    @Deployment(testable = false)
-//    @TargetsContainer("wildfly-arquillian")
+    @Deployment(testable = false)
     public static WebArchive createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class).
                 addClass(TestServlet.class).
                 addAsWebInfResource((new File(WEBAPP_SRC + "/WEB-INF", "web.xml")));
-        System.out.println(war.toString(true));
         return war;
     }
 
@@ -45,8 +52,8 @@ public class TestServletTest {
     @Test
     public void testProcessRequest() throws IOException, SAXException {
         WebConversation conv = new WebConversation();
-        GetMethodWebRequest getRequest = new GetMethodWebRequest("http://localhost:8080/metadata-complete/TestServlet");
+        GetMethodWebRequest getRequest = new GetMethodWebRequest(base + "/TestServlet");
         WebResponse getResponse = conv.getResponse(getRequest);
-        System.out.println(getResponse.getText());
+        assertTrue(getResponse.getText().contains("<title>Servlet url-pattern in web.xml</title>"));
     }
 }
