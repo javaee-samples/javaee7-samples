2816c6cbe4e8cdfd1317a4eb48ace207fd380d65
Arun Gupta <arun.gupta@gmail.com>
2013-11-21 21:45:51 -0800

    Adding new Arquillian tests

--- jaxrs/readerwriter/src/main/java/org/javaee7/jaxrs/readerwriter/MyResource.java
+++ jaxrs/readerwriter/src/main/java/org/javaee7/jaxrs/readerwriter/MyResource.java
@@ -51,16 +51,17 @@ public class MyResource {
     private final String[] response = { "apple", "banana", "mango" };
     
     @POST
-    @Consumes(value=MyObject.MIME_TYPE)
-    public String getFruit(MyObject mo) {
+    @Consumes(MyObject.MIME_TYPE)
+    public String postWithCustomMimeType(MyObject mo) {
         System.out.println("endpoint invoked (getFruit(" + mo.getIndex() + "))");
         
         return response[Integer.valueOf(mo.getIndex()) % 3];
     }
     
     @POST
-    @Path("fruitInt")
-    public String getFruit2(int index) {
+    @Path("index")
+    @Consumes("text/plain")
+    public String postSimple(int index) {
         return response[index % 3];
     }
 }
