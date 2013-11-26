710744dca83110e5e31e03c127c7b2611acbd5ac
arun-gupta <arun.gupta@gmail.com>
2013-11-26 15:17:32 -0800

    Missed a file

---
+++ concurrency/managedexecutor/src/main/java/org/javaee7/concurrency/managedexecutor/TestStatus.java
@@ -0,0 +1,10 @@
+package org.javaee7.concurrency.managedexecutor;
+
+/**
+ * @author Arun Gupta
+ */
+public class TestStatus {
+    public static boolean invokedRunnable;
+    public static boolean invokedTaskWithListener;
+    public static boolean invokedTaskWithTransaction;
+}
