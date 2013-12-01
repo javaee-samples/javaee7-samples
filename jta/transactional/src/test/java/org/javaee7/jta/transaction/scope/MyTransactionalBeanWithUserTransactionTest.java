3f23d693c3211d8c43fe1803f4fee88e16af6139
Alexis Hassler <alexis@sewatech.org>
2013-12-01 01:35:40 +0100

    Merging jta/transaction-scope and jta/transactional modules

--- jta/transactional/src/test/java/org/javaee7/jta/transaction/scope/MyTransactionalBeanWithUserTransactionTest.java
+++ jta/transactional/src/test/java/org/javaee7/jta/transaction/scope/MyTransactionalBeanWithUserTransactionTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import javax.enterprise.context.ContextNotActiveException;
 import javax.inject.Inject;
+import javax.transaction.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -20,40 +21,47 @@ import static org.junit.Assert.fail;
  * @author Alexis Hassler
  */
 @RunWith(Arquillian.class)
-public class MyTransactionalBeanTest {
+public class MyTransactionalBeanWithUserTransactionTest {
     @Deployment
     public static Archive<?> deploy() {
         return ShrinkWrap.create(JavaArchive.class)
-                         .addClasses(MyTransactionalBean.class, MyBean.class)
+                         .addClasses(MyTransactionalBean.class, MyTransactionScopedBean.class)
                          .addAsManifestResource("beans.xml");
     }
 
     @Inject MyTransactionalBean bean;
 
+    @Inject UserTransaction ut;
+
     @Test
-    public void should_withTransaction_have_only_one_instance_injected() {
+    public void should_withTransaction_have_only_one_instance_injected() throws Exception{
+        ut.begin();
         bean.withTransaction();
+        ut.commit();
         assertThat("bean1 and bean2 should the same object", bean.id1, is(bean.id2));
     }
 
     @Test
-    public void should_withTransaction_called_twice_have_different_instances_injected() {
+    public void should_withTransaction_called_twice_have_same_instances_injected() throws Exception {
+        ut.begin();
         bean.withTransaction();
         String firstId1 = bean.id1;
 
         bean.withTransaction();
         String secondId1 = bean.id1;
+        ut.commit();
 
-        assertThat("bean1 should change between scenarios", firstId1, is(not(secondId1)));
+        assertThat("bean1 should change between scenarios", firstId1, is(secondId1));
     }
 
     @Test
-    public void should_withoutTransaction_fail() {
+    public void should_withoutTransaction_NOT_fail() throws Exception {
         try {
+            ut.begin();
             bean.withoutTransaction();
-            fail("No ContextNotActiveException");
+            ut.commit();
         } catch (ContextNotActiveException e) {
+            fail(e.toString());
         }
     }
-
 }
