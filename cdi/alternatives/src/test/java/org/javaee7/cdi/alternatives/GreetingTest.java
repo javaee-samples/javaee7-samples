cbf04ce989fc8d637a9cde5ffa27cd2449c6844f
Alexis Hassler <alexis@sewatech.org>
2013-12-01 20:10:50 +0100

    Adding Arquillian tests to cdi/alternatives example

---
+++ cdi/alternatives/src/test/java/org/javaee7/cdi/alternatives/GreetingTest.java
@@ -0,0 +1,42 @@
+package org.javaee7.cdi.alternatives;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexis Hassler
+ */
+@RunWith(Arquillian.class)
+public class GreetingTest {
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClasses(Greeting.class, SimpleGreeting.class, FancyGreeting.class)
+                .addAsManifestResource("beans.xml");
+    }
+
+    @Inject Greeting bean;
+
+    @Test
+    public void should_bean_be_injected() throws Exception {
+        assertThat(bean, is(notNullValue()));
+    }
+
+    @Test
+    public void should_bean_be_fancy() throws Exception {
+        // because it is declared as the alternative in beans.xml
+        assertThat(bean, instanceOf(FancyGreeting.class));
+    }
+}
