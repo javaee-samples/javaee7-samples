7d9e277cd1e4932b2132f0248dba3b9508672342
Alexis Hassler <alexis@sewatech.org>
2013-12-01 20:23:21 +0100

    Adding Arquillian tests to cdi/bean-discovery-annotated example

---
+++ cdi/bean-discovery-annotated/src/test/java/org/javaee7/cdi/bean/discovery/annotated/GreetingTest.java
@@ -0,0 +1,41 @@
+package org.javaee7.cdi.bean.discovery.annotated;
+
+import org.hamcrest.CoreMatchers;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexis Hassler
+ */
+@RunWith(Arquillian.class)
+public class GreetingTest {
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(JavaArchive.class)
+                         .addClasses(Greeting.class, SimpleGreeting.class)
+                         .addAsManifestResource("beans.xml");
+    }
+
+    @Inject Greeting bean;
+
+    @Test
+    public void should_bean_be_injected() throws Exception {
+        assertThat(bean, is(CoreMatchers.notNullValue()));
+    }
+    @Test
+    public void should_bean_be_simple() throws Exception {
+        // because SimpleGreeting is annotated (scope)
+        assertThat(bean, instanceOf(SimpleGreeting.class));
+    }
+}
