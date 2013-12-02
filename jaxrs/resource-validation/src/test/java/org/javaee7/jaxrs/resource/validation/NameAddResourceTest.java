7d689a2e61afded38c40ac5cadfdfd35ce0d5604
dagguh <dagguh@gmail.com>
2013-12-02 20:31:53 +0100

    Test case for successful Name valdiation. Test case for failed Name validation, because of empty firstName.

--- jaxrs/resource-validation/src/test/java/org/javaee7/jaxrs/resource/validation/NameAddResourceTest.java
+++ jaxrs/resource-validation/src/test/java/org/javaee7/jaxrs/resource/validation/NameAddResourceTest.java
@@ -1,6 +1,8 @@
 package org.javaee7.jaxrs.resource.validation;
 
 import java.net.URL;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -21,30 +23,49 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class NameAddResourceTest {
 
-	@Deployment
+	@Deployment(testable = false)
 	public static WebArchive createDeployment() {
 		return ShrinkWrap.create(WebArchive.class)
-				.addClasses(MyApplication.class, NameAddResource.class, Name.class, Email.class, JsonObjectProvider.class, JsonStructureProvider.class);
+				.addClasses(MyApplication.class, NameAddResource.class, Name.class, Email.class, EmailValidator.class);
 	}
 	@ArquillianResource
 	private URL base;
 
 	@Test
 	public void shouldPassNameValidation() throws Exception {
-		if (true) {
-
-		return;
-		}
 		Client client = ClientBuilder.newClient();
 		client.register(JsonObjectProvider.class, JsonStructureProvider.class);
 		WebTarget target = client
 				.target(new URL(base, "webresources/nameadd").toExternalForm());
+		JsonObject name = Json.createObjectBuilder()
+				.add("firstName", "Sheldon")
+				.add("lastName", "Cooper")
+				.add("email", "random@example.com")
+				.build();
 
 		Response r = target
 				.request()
-				.post(Entity.json(new Name("Sheldon", "Cooper", "sheldon@cooper.com")));
+				.post(Entity.json(name));
 
 		assertEquals(r.getStatusInfo().getFamily(), Status.Family.SUCCESSFUL);
+	}
+	@Test
+	public void shouldFailAtFirstNameValidation() throws Exception {
+		Client client = ClientBuilder.newClient();
+		client.register(JsonObjectProvider.class, JsonStructureProvider.class);
+		WebTarget target = client
+				.target(new URL(base, "webresources/nameadd").toExternalForm());
+		JsonObject name = Json.createObjectBuilder()
+				.add("firstName", "")
+				.add("lastName", "Cooper")
+				.add("email", "random@example.com")
+				.build();
+
+		Response r = target
+				.request()
+				.post(Entity.json(name));
+
+		assertEquals(r.getStatusInfo().getFamily(), Status.Family.CLIENT_ERROR);
 	}
 
 }
