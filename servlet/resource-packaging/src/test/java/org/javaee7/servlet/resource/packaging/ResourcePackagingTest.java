b8325fcc027416224f455ca16b2efa9048bea725
Aslak Knutsen <aslak@redhat.com>
2013-12-04 09:33:58 +0100

    Change to use Client.target(URI) to be compatible with JAX-RS 1.0

--- servlet/resource-packaging/src/test/java/org/javaee7/servlet/resource/packaging/ResourcePackagingTest.java
+++ servlet/resource-packaging/src/test/java/org/javaee7/servlet/resource/packaging/ResourcePackagingTest.java
@@ -1,5 +1,21 @@
 package org.javaee7.servlet.resource.packaging;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
@@ -7,20 +23,6 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.junit.Assert.assertThat;
 
 /**
  * @author Jakub Marchwicki
@@ -40,7 +42,7 @@ public class ResourcePackagingTest {
     @Test
     public void getMyResourceJarStyles() throws MalformedURLException {
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target(new URL(base, "styles.css").toExternalForm());
+        WebTarget target = client.target(URI.create(new URL(base, "styles.css").toExternalForm()));
         Response response = target.request().get();
 
         assertThat(response.getStatus(), is(equalTo(200)));
