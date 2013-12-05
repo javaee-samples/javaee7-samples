08b86b74ab538039fe880476babc270255d51269
arun-gupta <arun.gupta@gmail.com>
2013-12-05 13:02:54 -0800

    Adding a convenient method to receive all messages (not used any where for now)

--- jms/send-receive/src/main/java/org/javaee7/jms/send/receive/simple/MessageReceiverSync.java
+++ jms/send-receive/src/main/java/org/javaee7/jms/send/receive/simple/MessageReceiverSync.java
@@ -39,11 +39,15 @@
  */
 package org.javaee7.jms.send.receive.simple;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
 import javax.jms.Queue;
+import javax.jms.QueueBrowser;
 
 import org.javaee7.jms.send.receive.Resources;
 
@@ -62,5 +66,18 @@ public class MessageReceiverSync {
 
     public String receiveMessage() {
         return context.createConsumer(myQueue).receiveBody(String.class, 1000);
+    }
+    
+    public void receiveAll() {
+        System.out.println("--> Receiving redundant messages ...");
+        try {
+            QueueBrowser browser = context.createBrowser(myQueue);
+            while (browser.getEnumeration().hasMoreElements()) {
+                System.out.println("--> here is one");
+                context.createConsumer(myQueue).receiveBody(String.class, 1000);
+            }
+        } catch (JMSException ex) {
+            Logger.getLogger(MessageReceiverSync.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
