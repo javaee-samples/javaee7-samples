dc8765bd0045b8430bb9f7b58eceb6e345ef21b5
arun-gupta <arun.gupta@gmail.com>
2013-12-05 13:03:15 -0800

    Adding a new test to send/receive multiple messages

--- jms/send-receive/src/test/java/org/javaee7/jms/send/receive/SyncTest.java
+++ jms/send-receive/src/test/java/org/javaee7/jms/send/receive/SyncTest.java
@@ -1,5 +1,6 @@
 package org.javaee7.jms.send.receive;
 
+import java.util.Arrays;
 import org.javaee7.jms.send.receive.classic.ClassicMessageSender;
 import org.javaee7.jms.send.receive.classic.ClassicMessageReceiver;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
@@ -40,7 +42,7 @@ public class SyncTest {
 
     @EJB
     MessageReceiverAppManaged appManagedReceiver;
-
+    
     @Test
     public void testClassicApi() {
         String message = "The test message over JMS 1.1 API";
@@ -63,6 +65,20 @@ public class SyncTest {
         appManagedSender.sendMessage(message);
 
         assertEquals(message, appManagedReceiver.receiveMessage());
+    }
+
+    @Test
+    public void testMultipleSendAndReceive() {
+        simpleSender.sendMessage("1");
+        simpleSender.sendMessage("2");
+        assertEquals("1", simpleReceiver.receiveMessage());
+        assertEquals("2", simpleReceiver.receiveMessage());
+        simpleSender.sendMessage("3");
+        simpleSender.sendMessage("4");
+        simpleSender.sendMessage("5");
+        assertEquals("3", simpleReceiver.receiveMessage());
+        assertEquals("4", simpleReceiver.receiveMessage());
+        assertEquals("5", simpleReceiver.receiveMessage());
     }
 
     @Deployment
