f8acc00a2faa3172294a362e93722374f35f94b2
arun-gupta <arun.gupta@gmail.com>
2013-12-07 06:47:10 -0800

    Adding a test for table generation using JPA metadata

--- jpa/schema-gen-metadata/src/main/java/org/javaee7/jpa/schemagen/metadata/Employee.java
+++ jpa/schema-gen-metadata/src/main/java/org/javaee7/jpa/schemagen/metadata/Employee.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.javaee7.jpa.schemagen;
+package org.javaee7.jpa.schemagen.metadata;
 
 import java.io.Serializable;
 import javax.persistence.Column;
