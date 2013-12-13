7974d43e6f28570866e9ab225bc17f3a9ea4f2bf
arun-gupta <arun.gupta@gmail.com>
2013-12-13 05:39:19 -0800

    Using HtmlUnit instead of HttpUnit

--- servlet/metadata-complete/src/test/java/org/javaee7/servlet/metadata/complete/TestServletTest.java
+++ servlet/metadata-complete/src/test/java/org/javaee7/servlet/metadata/complete/TestServletTest.java
@@ -1,8 +1,7 @@
 package org.javaee7.servlet.metadata.complete;
 
-import com.meterware.httpunit.GetMethodWebRequest;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebResponse;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -11,7 +10,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.xml.sax.SAXException;
@@ -27,12 +26,6 @@ public class TestServletTest {
     @ArquillianResource
     private URL base;
 
-    /**
-     * Arquillian specific method for creating a file which can be deployed
-     * while executing the test.
-     *
-     * @return a war file
-     */
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         WebArchive war = ShrinkWrap.create(WebArchive.class).
@@ -45,10 +38,9 @@ public class TestServletTest {
      * Test of processRequest method, of class TestServlet.
      */
     @Test
-    public void testProcessRequest() throws IOException, SAXException {
-        WebConversation conv = new WebConversation();
-        GetMethodWebRequest getRequest = new GetMethodWebRequest(base + "/TestServlet");
-        WebResponse getResponse = conv.getResponse(getRequest);
-        assertTrue(getResponse.getText().contains("<title>Servlet url-pattern in web.xml</title>"));
+    public void testGet() throws IOException, SAXException {
+        WebClient webClient = new WebClient();
+        HtmlPage page = webClient.getPage(base + "/TestServlet");
+        assertEquals("Servlet url-pattern in web.xml", page.getTitleText());
     }
 }
