f8806ebe3f28a4b1eb18cf1712932475a70e8de5
Roberto Cortez <radcortez@yahoo.com>
2013-12-31 11:40:39 +0000

    Added test for split project.

---
+++ batch/split/src/test/java/org/javaee7/batch/split/BatchSplitTest.java
@@ -0,0 +1,61 @@
+package org.javaee7.batch.split;
+
+import org.javaee7.util.BatchTestHelper;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.BatchStatus;
+import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.StepExecution;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Roberto Cortez
+ */
+@RunWith(Arquillian.class)
+public class BatchSplitTest {
+    @Deployment
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class)
+                .addClass(BatchTestHelper.class)
+                .addPackage("org.javaee7.batch.split")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
+                .addAsResource("META-INF/batch-jobs/myJob.xml");
+        System.out.println(war.toString(true));
+        return war;
+    }
+
+    @Test
+    public void testBatchSplit() throws Exception {
+        JobOperator jobOperator = BatchRuntime.getJobOperator();
+        Long executionId = jobOperator.start("myJob", new Properties());
+        JobExecution jobExecution = jobOperator.getJobExecution(executionId);
+
+        BatchTestHelper.keepTestAlive(jobExecution);
+
+        List<StepExecution> stepExecutions = jobOperator.getStepExecutions(executionId);
+        List<String> executedSteps = new ArrayList<>();
+        for (StepExecution stepExecution : stepExecutions) {
+            executedSteps.add(stepExecution.getStepName());
+        }
+
+        assertEquals(3, stepExecutions.size());
+        assertTrue(executedSteps.contains("step1"));
+        assertTrue(executedSteps.contains("step2"));
+        assertTrue(executedSteps.contains("step3"));
+        assertEquals(BatchStatus.COMPLETED, jobExecution.getBatchStatus());
+    }
+}
