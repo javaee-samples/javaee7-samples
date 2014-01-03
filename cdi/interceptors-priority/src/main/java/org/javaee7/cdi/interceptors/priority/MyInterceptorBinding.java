f80087a5976ce15e27e395fdd215c3729bba7416
Radim Hanus <radim.hanus@gmail.com>
2014-01-03 15:33:54 +0100

    fixed module name to interceptors-priority, removed Oracle copyright

---
+++ cdi/interceptors-priority/src/main/java/org/javaee7/cdi/interceptors/priority/MyInterceptorBinding.java
@@ -0,0 +1,20 @@
+package org.javaee7.cdi.interceptors.priority;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author Arun Gupta
+ */
+@Inherited
+@InterceptorBinding
+@Retention(RUNTIME)
+@Target({METHOD, TYPE})
+public @interface MyInterceptorBinding {
+}
