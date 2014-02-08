382225a6f2df9b007dea119fd00d05120c29167e
OndraZizka <zizka@seznam.cz>
2014-02-08 16:46:58 +0100

    Fix package name :)

--- ejb/singleton/src/main/java/org/javaee7/ejb/singleton/MySingletonBeanManagedConcurrency.java
+++ ejb/singleton/src/main/java/org/javaee7/ejb/singleton/MySingletonBeanManagedConcurrency.java
@@ -37,7 +37,7 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-package org.javaee7.ejb.stateless;
+package org.javaee7.ejb.singleton;
 
 import java.util.Date;
 import javax.annotation.PostConstruct;
