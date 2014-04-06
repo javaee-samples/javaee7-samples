5ebd872c90ce923c275e5ebeca145e36cd27aad2
abhirockzz <abhirockzz@gmail.com>
2014-04-06 15:20:56 +0530

    added correct scope annotation included one line description

--- cdi/scopes/src/main/java/org/javaee7/cdi/bean/scopes/MyApplicationScopedBean.java
+++ cdi/scopes/src/main/java/org/javaee7/cdi/bean/scopes/MyApplicationScopedBean.java
@@ -39,12 +39,15 @@
  */
 package org.javaee7.cdi.bean.scopes;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ApplicationScoped;
 
 /**
  * @author Arun Gupta
+ * 
+ * This class represents an Application Scoped CDI bean. Once injected, the container will hold on to the instance of this bean until
+ * the application itself terminates (server restart/redeployment of the application).
  */
-@RequestScoped
+@ApplicationScoped
 public class MyApplicationScopedBean {
     public String getID() {
         return this + "";
