339c030c6a179fec9acce49c948cf3b9a5ecc6d4
Roberto Cortez <radcortez@yahoo.com>
2014-05-08 12:07:06 +0100

    Fixed Chunk Exception Wildfly Test.

--- batch/chunk-exception/src/main/java/org/javaee7/batch/chunk/exception/MyItemWriter.java
+++ batch/chunk-exception/src/main/java/org/javaee7/batch/chunk/exception/MyItemWriter.java
@@ -48,11 +48,14 @@ import java.util.List;
  */
 @Named
 public class MyItemWriter extends AbstractItemWriter {
+    private static int retries = 0;
 
     @Override
     public void writeItems(List list) {
-        if (list.contains(new MyOutputRecord(8))) {
-            throw new IllegalArgumentException();
+        if (retries <= 3 && list.contains(new MyOutputRecord(8))) {
+            retries ++;
+            System.out.println("Throw UnsupportedOperationException in MyItemWriter");
+            throw new UnsupportedOperationException();
         }
 
         System.out.println("MyItemWriter.writeItems: " + list);
