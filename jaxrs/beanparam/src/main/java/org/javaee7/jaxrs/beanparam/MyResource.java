2545e94b9b4ed6aebdf8e69e04c06f506150f973
Xavier Coulon <xcoulon@redhat.com>
2014-05-26 14:46:12 +0200

    Adding JAX-RS @BeanParam example

---
+++ jaxrs/beanparam/src/main/java/org/javaee7/jaxrs/beanparam/MyResource.java
@@ -0,0 +1,22 @@
+package org.javaee7.jaxrs.beanparam;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Xavier Coulon
+ */
+@Path("/endpoint")
+public class MyResource {
+
+    @GET()
+    @Path("/{id1}/{id2}")
+    @Produces(MediaType.TEXT_PLAIN) 
+    public String get(@BeanParam MyPathParams pathParams, @BeanParam MyQueryParams queryParams) {
+		return "/" + pathParams.getId1() + "/" + pathParams.getId2() + "?param1=" + queryParams.getParam1() + "&param2="
+				+ queryParams.getParam2() + "&param3=" + queryParams.getParam3();
+    }
+}
