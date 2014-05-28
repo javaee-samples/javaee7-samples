0ec19817c8358fa6659611b16f5ac0979fb62bbf
Xavier Coulon <xcoulon@redhat.com>
2014-05-28 12:00:03 +0200

    JAX-RS ParamConverterProvider sample project

---
+++ jaxrs/paramconverter/src/main/java/org/javaee7/jaxrs/paramconverter/MyBean.java
@@ -0,0 +1,30 @@
+package org.javaee7.jaxrs.paramconverter;
+
+/**
+ * @author Xavier Coulon
+ *
+ */
+public class MyBean {
+	
+	private String value;
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return getValue();
+	}
+	
+}
---
+++ jaxrs/paramconverter/src/main/java/org/javaee7/jaxrs/paramconverter/MyConverterProvider.java
@@ -0,0 +1,42 @@
+package org.javaee7.jaxrs.paramconverter;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Xavier Coulon
+ *
+ */
+@Provider
+public class MyConverterProvider implements ParamConverterProvider {
+
+	@Override
+	public <T> ParamConverter<T> getConverter(final Class<T> rawType, final Type genericType,
+            final Annotation[] annotations) {
+		if (rawType.getName().equals(MyBean.class.getName())) {
+			return new ParamConverter<T>() {
+
+				@Override
+				public T fromString(String value) {
+					MyBean myBean = new MyBean();
+					myBean.setValue(value);
+					return rawType.cast(myBean);
+				}
+
+				@Override
+				public String toString(T myBean) {
+					if (myBean == null) {
+						return null;
+					}
+					return myBean.toString();
+				}
+			};
+		}
+		return null;
+	}
+
+}
