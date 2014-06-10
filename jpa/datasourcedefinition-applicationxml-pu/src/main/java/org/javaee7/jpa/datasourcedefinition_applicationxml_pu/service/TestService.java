e9210a39b1f27e1a5450ca0518e79cec5f2ccf7b
arjan tijms <arjan.tijms@gmail.com>
2014-06-10 09:01:43 +0200

    Test that data-source can be defined in application.xml and used in persistence.xml.

---
+++ jpa/datasourcedefinition-applicationxml-pu/src/main/java/org/javaee7/jpa/datasourcedefinition_applicationxml_pu/service/TestService.java
@@ -0,0 +1,40 @@
+package org.javaee7.jpa.datasourcedefinition_applicationxml_pu.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.javaee7.jpa.datasourcedefinition_applicationxml_pu.entity.TestEntity;
+
+/**
+ * This service does some JPA operations. The purpose of this entire test
+ * is just to see whether the data source can be used so the actual operations
+ * don't matter much.
+ * 
+ * @author Arjan Tijms
+ *
+ */
+@Stateless
+public class TestService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void saveNewEntity() {
+        
+        TestEntity testEntity = new TestEntity();
+        testEntity.setValue("mytest");
+        
+        entityManager.persist(testEntity);
+    }
+    
+    public List<TestEntity> getAllEntities() {
+        return entityManager.createQuery("SELECT _testEntity FROM TestEntity _testEntity", TestEntity.class)
+                            .getResultList();
+    }
+
+    
+
+}
