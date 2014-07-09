81833c05640f31bc575e2e4a2571a59fdb91f840
Roberto Cortez <radcortez@yahoo.com>
2014-07-09 02:14:13 +0100

    Added tests to Scheduling project.

---
+++ batch/scheduling/src/main/java/org/javaee7/batch/samples/scheduling/AbstractTimerBatch.java
@@ -0,0 +1,22 @@
+package org.javaee7.batch.samples.scheduling;
+
+import javax.batch.runtime.BatchRuntime;
+import javax.ejb.Schedule;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @author Roberto Cortez
+ */
+public abstract class AbstractTimerBatch {
+    public static List<Long> executedBatchs = new ArrayList<>();
+
+    @Schedule(hour = "*", minute = "0", second = "0")
+    public void myJob() {
+        executedBatchs.add(BatchRuntime.getJobOperator().start("myJob", new Properties()));
+        afterRun();
+    }
+
+    protected void afterRun() {}
+}
---
+++ batch/scheduling/src/main/java/org/javaee7/batch/samples/scheduling/MyManagedScheduledBatchBean.java
@@ -0,0 +1,71 @@
+package org.javaee7.batch.samples.scheduling;
+
+import javax.annotation.Resource;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.BatchStatus;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.enterprise.concurrent.LastExecution;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
+import javax.enterprise.concurrent.Trigger;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author arungupta
+ */
+@Stateless
+@Local(MyManagedScheduledBatch.class)
+public class MyManagedScheduledBatchBean implements MyManagedScheduledBatch {
+    @Resource
+    private ManagedScheduledExecutorService executor;
+
+    @Override
+    public void runJob() {
+        executor.schedule(createJob(), new Trigger() {
+
+            @Override
+            public Date getNextRunTime(LastExecution lastExecutionInfo, Date taskScheduledTime) {
+                if (MyJob.executedBatchs.size() >= 3) {
+                    return null;
+                }
+
+                Calendar cal = Calendar.getInstance();
+
+                if (lastExecutionInfo == null) {
+                    cal.setTime(taskScheduledTime);
+                } else {
+                    cal.setTime(lastExecutionInfo.getRunStart());
+                }
+
+                cal.add(Calendar.SECOND, 10);
+                return cal.getTime();
+            }
+
+            @Override
+            public boolean skipRun(LastExecution lastExecutionInfo, Date scheduledRunTime) {
+                List<Long> executedBatchs = MyJob.executedBatchs;
+
+                for (Long executedBatch : executedBatchs) {
+                    if (!BatchRuntime.getJobOperator().getJobExecution(executedBatch).getBatchStatus().equals(
+                            BatchStatus.COMPLETED)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+        });
+    }
+
+    public void runJob2() {
+        executor.scheduleWithFixedDelay(new MyJob(), 1, 2, TimeUnit.MINUTES);
+    }
+
+    protected MyJob createJob() {
+        return new MyJob();
+    }
+}
