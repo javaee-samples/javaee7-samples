973179879f65d7f5b50c56f12485431afb1791e4
arun-gupta <arun.gupta@gmail.com>
2014-07-10 14:57:15 -0700

    adding new tests for JAXRS/Bean Validation

--- jaxrs/beanvalidation/src/main/java/org/javaee7/jaxrs/beanvalidation/MyResource.java
+++ jaxrs/beanvalidation/src/main/java/org/javaee7/jaxrs/beanvalidation/MyResource.java
@@ -39,7 +39,12 @@
  */
 package org.javaee7.jaxrs.beanvalidation;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -55,5 +60,10 @@ public class MyResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String post(@Size(min = 3) String payload) {
         return payload;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void post2(@NotNull @FormParam("name")String name, @Min(1) @Max(10)@FormParam("age")int age) {
     }
 }
--- jaxrs/beanvalidation/src/test/java/org/javaee7/jaxrs/beanvalidation/MyResourceTest.java
+++ jaxrs/beanvalidation/src/test/java/org/javaee7/jaxrs/beanvalidation/MyResourceTest.java
@@ -18,6 +18,7 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MultivaluedHashMap;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -36,8 +37,8 @@ public class MyResourceTest {
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-       return ShrinkWrap.create(WebArchive.class)
-             .addClasses(MyApplication.class, MyResource.class);
+        return ShrinkWrap.create(WebArchive.class)
+                .addClasses(MyApplication.class, MyResource.class);
     }
     private static WebTarget target;
 
@@ -64,6 +65,55 @@ public class MyResourceTest {
     public void testValidRequest() {
         String r = target.request().post(Entity.text("foo"), String.class);
         assertEquals("foo", r);
+    }
+
+    @Test
+    public void testValidPostRequest() {
+        MultivaluedHashMap<String, String> map = new MultivaluedHashMap<>();
+        map.add("name", "Penny");
+        map.add("age", "1");
+        target.request().post(Entity.form(map));
+
+        map.clear();
+        map.add("name", "Leonard");
+        map.add("age", "2");
+        target.request().post(Entity.form(map));
+    }
+
+    @Test
+    public void testInvalidPostRequest() {
+        try {
+            MultivaluedHashMap<String, String> map = new MultivaluedHashMap<>();
+            map.add("name", null);
+            map.add("age", "1");
+            target.request().post(Entity.form(map));
+        } catch (BadRequestException e) {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void testInvalidPostRequestLesserAge() {
+        try {
+            MultivaluedHashMap<String, String> map = new MultivaluedHashMap<>();
+            map.add("name", "Penny");
+            map.add("age", "0");
+            target.request().post(Entity.form(map));
+        } catch (BadRequestException e) {
+            assertNotNull(e);
+        }
+    }
+
+    @Test
+    public void testInvalidPostRequestGreaterAge() {
+        try {
+            MultivaluedHashMap<String, String> map = new MultivaluedHashMap<>();
+            map.add("name", "Penny");
+            map.add("age", "11");
+            target.request().post(Entity.form(map));
+        } catch (BadRequestException e) {
+            assertNotNull(e);
+        }
     }
 
 }
