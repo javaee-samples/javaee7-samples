a1a296df82a39d8af8b40ccbf82adb91525b2699
arun-gupta <arun.gupta@gmail.com>
2014-07-09 18:05:03 -0700

    adding a new test for default datasource

---
+++ jpa/default-datasource/src/main/java/org/javaee7/jpa/defaultdatasource/EmployeeBean.java
@@ -0,0 +1,24 @@
+package org.javaee7.jpa.defaultdatasource;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author Arun Gupta
+ */
+@Stateless
+public class EmployeeBean {
+
+    @PersistenceContext
+    EntityManager em;
+    
+    public void persist(Employee e) {
+        em.persist(e);
+    }
+    
+    public List<Employee> get() {
+        return em.createNamedQuery("Employee.findAll", Employee.class).getResultList();
+    }
+}
