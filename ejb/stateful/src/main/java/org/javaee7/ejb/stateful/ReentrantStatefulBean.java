3d30dd7de8a95bce0135876a5a50ce843fb02a70
arjan tijms <arjan.tijms@gmail.com>
2014-10-06 00:05:59 +0200

    Fixed formatting

--- ejb/stateful/src/main/java/org/javaee7/ejb/stateful/ReentrantStatefulBean.java
+++ ejb/stateful/src/main/java/org/javaee7/ejb/stateful/ReentrantStatefulBean.java
@@ -7,20 +7,20 @@ import javax.ejb.Stateful;
 /**
  * 
  * @author Arjan Tijms
- *
+ * 
  */
 @Stateful
 public class ReentrantStatefulBean {
 
-	@Resource
-	private SessionContext sessionConext;
-	
-	public void initialMethod() {
-		sessionConext.getBusinessObject(ReentrantStatefulBean.class).reentrantMehthod();
-	}
-	
-	public void reentrantMehthod() {
-		
-	}
-	
+    @Resource
+    private SessionContext sessionConext;
+
+    public void initialMethod() {
+        sessionConext.getBusinessObject(ReentrantStatefulBean.class).reentrantMehthod();
+    }
+
+    public void reentrantMehthod() {
+
+    }
+
 }
