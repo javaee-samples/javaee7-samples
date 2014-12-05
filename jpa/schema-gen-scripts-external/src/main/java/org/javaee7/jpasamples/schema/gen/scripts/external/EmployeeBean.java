a90854ddc6ef7b32041f299a9b1ac317c9a7efeb
Roberto Cortez <radcortez@yahoo.com>
2014-12-05 00:03:10 +0000

    Added tests for schema-gen-scripts-external project.

--- jpa/schema-gen-scripts-external/src/main/java/org/javaee7/jpasamples/schema/gen/scripts/external/EmployeeBean.java
+++ jpa/schema-gen-scripts-external/src/main/java/org/javaee7/jpasamples/schema/gen/scripts/external/EmployeeBean.java
@@ -1,23 +1,18 @@
 package org.javaee7.jpasamples.schema.gen.scripts.external;
 
-import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  * @author Arun Gupta
  */
 @Stateless
 public class EmployeeBean {
-
     @PersistenceContext
-    EntityManager em;
-    
-    public void persist(Employee e) {
-        em.persist(e);
-    }
-    
+    private EntityManager em;
+
     public List<Employee> get() {
         return em.createNamedQuery("Employee.findAll", Employee.class).getResultList();
     }
