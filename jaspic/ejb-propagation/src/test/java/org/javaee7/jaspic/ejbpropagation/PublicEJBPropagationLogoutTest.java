4cc2cccd50765b7f5989b1b789a85817488b2422
arjan.tijms <arjan.tijms@kizitos.com>
2015-02-23 18:02:03 +0100

    Added test to see if a logout from the web propagates to EJB

---
+++ jaspic/ejb-propagation/src/test/java/org/javaee7/jaspic/ejbpropagation/PublicEJBPropagationLogoutTest.java
@@ -0,0 +1,62 @@
+package org.javaee7.jaspic.ejbpropagation;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+
+import org.javaee7.jaspic.common.ArquillianBase;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.xml.sax.SAXException;
+
+/**
+ * This tests that the established authenticated identity propagates correctly
+ * from the web layer to a "public" EJB (an EJB without declarative role
+ * checking) and that after logging out but still within the same request this
+ * identity is cleared.
+ * 
+ * @author Arjan Tijms
+ * 
+ */
+@RunWith(Arquillian.class)
+public class PublicEJBPropagationLogoutTest extends ArquillianBase {
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return defaultArchive();
+    }
+
+    @Test
+    public void testProtectedServletWithLoginCallingEJB() throws IOException, SAXException {
+
+        String response = getFromServerPath("public/servlet-public-ejb-logout?doLogin");
+
+        System.out.println(response);
+
+        // Both the web (HttpServletRequest) and EJB (EJBContext) should see the
+        // same
+        // user name.
+
+        assertTrue(response.contains("web username: test"));
+        assertTrue("Web has user principal set, but EJB not.", response.contains("EJB username: test"));
+
+        
+        // After logging out, both the web and EJB should no longer see the user
+        // name
+
+        assertFalse(
+            "Web module did not clear authenticated identity after logout", 
+            response.contains("web username after logout: test")
+        );
+        assertFalse(
+            "EJB did not clear authenticated identity after logout", 
+            response.contains("EJB username after logout: test")
+        );
+
+    }
+
+}
