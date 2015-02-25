9a6f1195b10184f16cba17f8ce5191281b5c8992
Aslak Knutsen <aslak@4fs.no>
2015-02-25 12:33:26 +0100

    Update to Groovy 2.3.0
    * Make Servlet Filter test testable = false

--- servlet/servlet-filters/src/test/groovy/org/javaee7/servlet/filters/FilterServletSpecification.groovy
+++ servlet/servlet-filters/src/test/groovy/org/javaee7/servlet/filters/FilterServletSpecification.groovy
@@ -1,23 +1,23 @@
 package org.javaee7.servlet.filters
 
-import org.jboss.arquillian.container.test.api.Deployment
-import org.jboss.arquillian.container.test.api.RunAsClient
-import org.jboss.arquillian.spock.ArquillianSputnik
-import org.jboss.arquillian.test.api.ArquillianResource
-import org.jboss.shrinkwrap.api.ShrinkWrap
-import org.jboss.shrinkwrap.api.spec.WebArchive
-import org.junit.runner.RunWith
-import spock.lang.Specification
-
 import javax.ws.rs.client.Client
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 import javax.ws.rs.core.Response
 
+import org.jboss.arquillian.container.test.api.Deployment
+import org.jboss.arquillian.spock.ArquillianSputnik
+import org.jboss.arquillian.test.api.ArquillianResource
+import org.jboss.shrinkwrap.api.ShrinkWrap
+import org.jboss.shrinkwrap.api.spec.WebArchive
+import org.junit.runner.RunWith
+
+import spock.lang.Specification
+
 @RunWith(ArquillianSputnik)
 class FilterServletSpecification extends Specification{
 
-    @Deployment
+    @Deployment(testable = false)
     def static WebArchive "create deployment"() {
         return ShrinkWrap.create(WebArchive.class)
                 .addClass(CharResponseWrapper.class)
@@ -27,7 +27,6 @@ class FilterServletSpecification extends Specification{
     @ArquillianResource
     private URL base;
 
-    @RunAsClient
     def "standard servlet should return a simple text"() {
         given:
         Client client = ClientBuilder.newClient();
@@ -40,7 +39,6 @@ class FilterServletSpecification extends Specification{
         response.readEntity(String.class) == "bar"
     }
 
-    @RunAsClient
     def "filtered servlet should return a enhanced foobar text"() {
         given:
         Client client = ClientBuilder.newClient();
