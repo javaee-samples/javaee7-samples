2b478e842bbf1814bf09f7a9f092900f4988561b
Roberto Cortez <radcortez@yahoo.com>
2015-08-19 13:20:33 +0100

    #319. Add the CDDL license back to the original list of files.

--- jaxws/jaxws-client/src/main/java/org/javaee7/jaxws/client/gen/ObjectFactory.java
+++ jaxws/jaxws-client/src/main/java/org/javaee7/jaxws/client/gen/ObjectFactory.java
@@ -1,9 +1,11 @@
+
 package org.javaee7.jaxws.client.gen;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlElementDecl;
 import javax.xml.bind.annotation.XmlRegistry;
 import javax.xml.namespace.QName;
+
 
 /**
  * This object contains factory methods for each 
@@ -22,16 +24,16 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _FindEBooks_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "findEBooks");
-    private final static QName _SaveBookResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "saveBookResponse");
-    private final static QName _WelcomeMessage_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "welcomeMessage");
-    private final static QName _FindEBooksResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "findEBooksResponse");
-    private final static QName _AddAppendix_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "addAppendix");
-    private final static QName _AddAppendixResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "addAppendixResponse");
-    private final static QName _TakeBook_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "takeBook");
-    private final static QName _WelcomeMessageResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "welcomeMessageResponse");
-    private final static QName _TakeBookResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "takeBookResponse");
     private final static QName _SaveBook_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "saveBook");
+    private final static QName _AddAppendix_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "addAppendix");
+    private final static QName _FindEBooksResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "findEBooksResponse");
+    private final static QName _TakeBook_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "takeBook");
+    private final static QName _AddAppendixResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "addAppendixResponse");
+    private final static QName _WelcomeMessageResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "welcomeMessageResponse");
+    private final static QName _FindEBooks_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "findEBooks");
+    private final static QName _WelcomeMessage_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "welcomeMessage");
+    private final static QName _SaveBookResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "saveBookResponse");
+    private final static QName _TakeBookResponse_QNAME = new QName("http://endpoint.jaxws.javaee7.org/", "takeBookResponse");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.javaee7.jaxws.client.gen
@@ -49,14 +51,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link TakeBookResponse }
-     * 
-     */
-    public TakeBookResponse createTakeBookResponse() {
-        return new TakeBookResponse();
-    }
-
-    /**
      * Create an instance of {@link FindEBooksResponse }
      * 
      */
@@ -65,35 +59,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link AddAppendix }
+     * Create an instance of {@link TakeBookResponse }
      * 
      */
-    public AddAppendix createAddAppendix() {
-        return new AddAppendix();
-    }
-
-    /**
-     * Create an instance of {@link FindEBooks }
-     * 
-     */
-    public FindEBooks createFindEBooks() {
-        return new FindEBooks();
-    }
-
-    /**
-     * Create an instance of {@link AddAppendixResponse }
-     * 
-     */
-    public AddAppendixResponse createAddAppendixResponse() {
-        return new AddAppendixResponse();
-    }
-
-    /**
-     * Create an instance of {@link WelcomeMessage }
-     * 
-     */
-    public WelcomeMessage createWelcomeMessage() {
-        return new WelcomeMessage();
+    public TakeBookResponse createTakeBookResponse() {
+        return new TakeBookResponse();
     }
 
     /**
@@ -102,14 +72,6 @@ public class ObjectFactory {
      */
     public TakeBook createTakeBook() {
         return new TakeBook();
-    }
-
-    /**
-     * Create an instance of {@link WelcomeMessageResponse }
-     * 
-     */
-    public WelcomeMessageResponse createWelcomeMessageResponse() {
-        return new WelcomeMessageResponse();
     }
 
     /**
@@ -129,39 +91,52 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link FindEBooks }{@code >}}
+     * Create an instance of {@link WelcomeMessageResponse }
      * 
      */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "findEBooks")
-    public JAXBElement<FindEBooks> createFindEBooks(FindEBooks value) {
-        return new JAXBElement<FindEBooks>(_FindEBooks_QNAME, FindEBooks.class, null, value);
+    public WelcomeMessageResponse createWelcomeMessageResponse() {
+        return new WelcomeMessageResponse();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link SaveBookResponse }{@code >}}
+     * Create an instance of {@link AddAppendixResponse }
      * 
      */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "saveBookResponse")
-    public JAXBElement<SaveBookResponse> createSaveBookResponse(SaveBookResponse value) {
-        return new JAXBElement<SaveBookResponse>(_SaveBookResponse_QNAME, SaveBookResponse.class, null, value);
+    public AddAppendixResponse createAddAppendixResponse() {
+        return new AddAppendixResponse();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link WelcomeMessage }{@code >}}
+     * Create an instance of {@link FindEBooks }
      * 
      */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "welcomeMessage")
-    public JAXBElement<WelcomeMessage> createWelcomeMessage(WelcomeMessage value) {
-        return new JAXBElement<WelcomeMessage>(_WelcomeMessage_QNAME, WelcomeMessage.class, null, value);
+    public FindEBooks createFindEBooks() {
+        return new FindEBooks();
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link FindEBooksResponse }{@code >}}
+     * Create an instance of {@link WelcomeMessage }
      * 
      */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "findEBooksResponse")
-    public JAXBElement<FindEBooksResponse> createFindEBooksResponse(FindEBooksResponse value) {
-        return new JAXBElement<FindEBooksResponse>(_FindEBooksResponse_QNAME, FindEBooksResponse.class, null, value);
+    public WelcomeMessage createWelcomeMessage() {
+        return new WelcomeMessage();
+    }
+
+    /**
+     * Create an instance of {@link AddAppendix }
+     * 
+     */
+    public AddAppendix createAddAppendix() {
+        return new AddAppendix();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SaveBook }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "saveBook")
+    public JAXBElement<SaveBook> createSaveBook(SaveBook value) {
+        return new JAXBElement<SaveBook>(_SaveBook_QNAME, SaveBook.class, null, value);
     }
 
     /**
@@ -174,12 +149,12 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link AddAppendixResponse }{@code >}}
+     * Create an instance of {@link JAXBElement }{@code <}{@link FindEBooksResponse }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "addAppendixResponse")
-    public JAXBElement<AddAppendixResponse> createAddAppendixResponse(AddAppendixResponse value) {
-        return new JAXBElement<AddAppendixResponse>(_AddAppendixResponse_QNAME, AddAppendixResponse.class, null, value);
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "findEBooksResponse")
+    public JAXBElement<FindEBooksResponse> createFindEBooksResponse(FindEBooksResponse value) {
+        return new JAXBElement<FindEBooksResponse>(_FindEBooksResponse_QNAME, FindEBooksResponse.class, null, value);
     }
 
     /**
@@ -192,6 +167,15 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link AddAppendixResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "addAppendixResponse")
+    public JAXBElement<AddAppendixResponse> createAddAppendixResponse(AddAppendixResponse value) {
+        return new JAXBElement<AddAppendixResponse>(_AddAppendixResponse_QNAME, AddAppendixResponse.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link WelcomeMessageResponse }{@code >}}
      * 
      */
@@ -201,21 +185,39 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link FindEBooks }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "findEBooks")
+    public JAXBElement<FindEBooks> createFindEBooks(FindEBooks value) {
+        return new JAXBElement<FindEBooks>(_FindEBooks_QNAME, FindEBooks.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link WelcomeMessage }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "welcomeMessage")
+    public JAXBElement<WelcomeMessage> createWelcomeMessage(WelcomeMessage value) {
+        return new JAXBElement<WelcomeMessage>(_WelcomeMessage_QNAME, WelcomeMessage.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SaveBookResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "saveBookResponse")
+    public JAXBElement<SaveBookResponse> createSaveBookResponse(SaveBookResponse value) {
+        return new JAXBElement<SaveBookResponse>(_SaveBookResponse_QNAME, SaveBookResponse.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link TakeBookResponse }{@code >}}
      * 
      */
     @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "takeBookResponse")
     public JAXBElement<TakeBookResponse> createTakeBookResponse(TakeBookResponse value) {
         return new JAXBElement<TakeBookResponse>(_TakeBookResponse_QNAME, TakeBookResponse.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link SaveBook }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://endpoint.jaxws.javaee7.org/", name = "saveBook")
-    public JAXBElement<SaveBook> createSaveBook(SaveBook value) {
-        return new JAXBElement<SaveBook>(_SaveBook_QNAME, SaveBook.class, null, value);
     }
 
 }
