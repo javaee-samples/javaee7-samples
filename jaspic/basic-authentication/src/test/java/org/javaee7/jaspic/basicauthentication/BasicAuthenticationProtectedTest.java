5a9b9d9749a63ec3311178e845fad0632e78c1a3
arjan.tijms <arjan.tijms@kizitos.com>
2015-11-05 19:16:27 +0100

    Added extra comments and failure explanations

--- jaspic/basic-authentication/src/test/java/org/javaee7/jaspic/basicauthentication/BasicAuthenticationProtectedTest.java
+++ jaspic/basic-authentication/src/test/java/org/javaee7/jaspic/basicauthentication/BasicAuthenticationProtectedTest.java
@@ -14,8 +14,8 @@ import org.junit.runner.RunWith;
 import org.xml.sax.SAXException;
 
 /**
- * This tests that we can login from a protected resource (a resource for which security constraints have been set) and then
- * access it.
+ * This tests that we can login from a protected resource (a resource for which
+ * security constraints have been set) and then access it.
  * 
  * @author Arjan Tijms
  * 
@@ -34,7 +34,10 @@ public class BasicAuthenticationProtectedTest extends ArquillianBase {
         String response = getFromServerPath("protected/servlet");
 
         // Not logged-in thus should not be accessible.
-        assertFalse(response.contains("This is a protected servlet"));
+        assertFalse(
+            "Not authenticated, so should not have been able to access protected resource",
+            response.contains("This is a protected servlet")
+        );
     }
 
     @Test
@@ -43,7 +46,40 @@ public class BasicAuthenticationProtectedTest extends ArquillianBase {
         String response = getFromServerPath("protected/servlet?doLogin=true");
 
         // Now has to be logged-in so page is accessible
-        assertTrue(response.contains("This is a protected servlet"));
+        assertTrue(
+            "Should have been authenticated, but could not access protected resource",
+            response.contains("This is a protected servlet")
+        );
+
+        // Not only does the page needs to be accessible, the caller should have
+        // the correct
+        // name and roles as well
+
+        // Being able to access a page protected by a role but then seeing the un-authenticated
+        // (anonymous) user would normally be impossible, but could happen if the authorization
+        // system checks roles on the authenticated subject, but does not correctly expose
+        // or propagate these to the HttpServletRequest
+        assertFalse(
+            "Protected resource could be accessed, but the user appears to be the unauthenticated user. " + 
+            "This should not be possible", 
+            response.contains("web username: null")
+        );
+        
+        // An authenticated user should have the exact name "test" and nothing else.
+        assertTrue(
+            "Protected resource could be accessed, but the username is not correct.",
+            response.contains("web username: test")
+        );
+
+        // Being able to access a page protected by role "architect" but failing
+        // the test for this role would normally be impossible, but could happen if the
+        // authorization system checks roles on the authenticated subject, but does not
+        // correctly expose or propagate these to the HttpServletRequest
+        assertTrue(
+            "Resource protected by role \"architect\" could be accessed, but user fails test for this role." + 
+            "This should not be possible", 
+            response.contains("web user has role \"architect\": true")
+        );
     }
 
 }
