e7a3b436d98a7be794a530465db2e9072362837c
arjan.tijms <arjan.tijms@kizitos.com>
2016-04-29 18:59:03 +0200

    Added JASPIC tests for EJB propagation after register session

---
+++ jaspic/register-session/src/main/java/org/javaee7/jaspic/registersession/ejb/PublicEJB.java
@@ -0,0 +1,27 @@
+package org.javaee7.jaspic.registersession.ejb;
+import javax.annotation.Resource;
+import javax.ejb.EJBContext;
+import javax.ejb.Stateless;
+
+/**
+ * This is a "public" EJB in the sense that all its methods should be accessible and there is no declarative role checking prior
+ * to accessing a method.
+ * 
+ * @author Arjan Tijms
+ * 
+ */
+@Stateless
+public class PublicEJB {
+
+    @Resource
+    private EJBContext ejbContext;
+
+    public String getUserName() {
+        try {
+            return ejbContext.getCallerPrincipal() != null ? ejbContext.getCallerPrincipal().getName() : null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ jaspic/register-session/src/main/java/org/javaee7/jaspic/registersession/servlet/PublicServletProtectedEJB.java
@@ -0,0 +1,63 @@
+package org.javaee7.jaspic.registersession.servlet;
+import static java.util.logging.Level.SEVERE;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.javaee7.jaspic.registersession.ejb.ProtectedEJB;
+
+
+/**
+ * 
+ * @author Arjan Tijms
+ * 
+ */
+@WebServlet(urlPatterns = "/public/servlet-protected-ejb")
+public class PublicServletProtectedEJB extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private final static Logger logger = Logger.getLogger(PublicServletProtectedEJB.class.getName());
+
+    @EJB
+    private ProtectedEJB protectedEJB;
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String webName = null;
+        if (request.getUserPrincipal() != null) {
+            webName = request.getUserPrincipal().getName();
+        }
+
+        String ejbName = "";
+        try {
+            ejbName = protectedEJB.getUserName();
+        } catch (Exception e) {
+            logger.log(SEVERE, "", e);
+        }
+
+        response.getWriter().write("web username: " + webName + "\n" + "EJB username: " + ejbName + "\n");
+
+        boolean webHasRole = request.isUserInRole("architect");
+        
+        boolean ejbHasRole = false;
+        try {
+            ejbHasRole = protectedEJB.isUserArchitect();
+        } catch (Exception e) {
+            logger.log(SEVERE, "", e);
+        }
+
+        response.getWriter().write(
+            "web user has role \"architect\": " + webHasRole + "\n" + "EJB user has role \"architect\": " + ejbHasRole
+                + "\n");
+
+    }
+
+}
