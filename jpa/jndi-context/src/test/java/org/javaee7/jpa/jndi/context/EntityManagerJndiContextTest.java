763005e4075a5e316144887976cce112eb527406
arjan tijms <arjan.tijms@gmail.com>
2017-05-04 14:35:18 +0200

    Fixed EntityManagerJndiContextTest

--- jpa/jndi-context/src/test/java/org/javaee7/jpa/jndi/context/EntityManagerJndiContextTest.java
+++ jpa/jndi-context/src/test/java/org/javaee7/jpa/jndi/context/EntityManagerJndiContextTest.java
@@ -1,34 +1,42 @@
 package org.javaee7.jpa.jndi.context;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
-import java.util.List;
 
-import static org.junit.Assert.*;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * @author Roberto Cortez
  */
 @RunWith(Arquillian.class)
 public class EntityManagerJndiContextTest {
+    
     @Inject
     private EmployeeBean employeeBean;
 
     @Deployment
     public static WebArchive createDeployment() {
-        WebArchive war = ShrinkWrap.create(WebArchive.class)
+        WebArchive war = create(WebArchive.class)
             .addPackage("org.javaee7.jpa.jndi.context")
             .addAsResource("META-INF/persistence.xml")
-            .addAsResource("META-INF/load.sql");
+            .addAsResource("META-INF/create.sql")
+            .addAsResource("META-INF/load.sql")
+            .addAsResource("META-INF/drop.sql");
+        
         System.out.println(war.toString(true));
+        
         return war;
     }
 
