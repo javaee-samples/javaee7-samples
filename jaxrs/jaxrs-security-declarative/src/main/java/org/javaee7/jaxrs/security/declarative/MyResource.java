c715338cbc6daf4c3219a64e9e600ea3273fbc59
arjan tijms <arjan.tijms@gmail.com>
2017-05-30 13:54:15 +0200

    Cleaned up jaxrs decl security test and updated to html unit

--- jaxrs/jaxrs-security-declarative/src/main/java/org/javaee7/jaxrs/security/declarative/MyResource.java
+++ jaxrs/jaxrs-security-declarative/src/main/java/org/javaee7/jaxrs/security/declarative/MyResource.java
@@ -1,6 +1,7 @@
 package org.javaee7.jaxrs.security.declarative;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
@@ -11,12 +12,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  * @author Arun Gupta
  */
-@Path("myresource")
 @RequestScoped
+@Path("myresource")
+@Produces(TEXT_PLAIN)
 public class MyResource {
 
     @GET
