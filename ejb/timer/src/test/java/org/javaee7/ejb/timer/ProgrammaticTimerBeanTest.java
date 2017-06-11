63182cefa82966d7749410467b8f6ebe1b5af171
arjan tijms <arjan.tijms@gmail.com>
2017-06-11 02:48:50 +0200

    Made EJB timer test more robust - guard against missed ping

--- ejb/timer/src/test/java/org/javaee7/ejb/timer/ProgrammaticTimerBeanTest.java
+++ ejb/timer/src/test/java/org/javaee7/ejb/timer/ProgrammaticTimerBeanTest.java
@@ -1,25 +1,21 @@
 package org.javaee7.ejb.timer;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.awaitility.Awaitility.to;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.is;
+import static org.javaee7.ejb.timer.WithinWindowMatcher.withinWindow;
+import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
+
+import javax.inject.Inject;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.inject.Inject;
-import java.io.File;
-
-import static com.jayway.awaitility.Awaitility.await;
-import static com.jayway.awaitility.Awaitility.to;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.javaee7.ejb.timer.WithinWindowMatcher.withinWindow;
 
 /**
  * author: Jacek Jackowiak
@@ -27,32 +23,38 @@ import static org.javaee7.ejb.timer.WithinWindowMatcher.withinWindow;
 @RunWith(Arquillian.class)
 public class ProgrammaticTimerBeanTest {
 
-    final static long TIMEOUT = 5000l;
-    final static long TOLERANCE = 1000l;
+    private static final long TIMEOUT = 5000l;
+    private static final long TOLERANCE = 1000l;
 
     @Inject
-    PingsListener pings;
+    private PingsListener pings;
 
     @Deployment
     public static WebArchive deploy() {
-        File[] jars = Maven.resolver().loadPomFromFile("pom.xml")
-                .resolve("com.jayway.awaitility:awaitility")
-                .withTransitivity().asFile();
-
-        return ShrinkWrap.create(WebArchive.class)
-                .addAsLibraries(jars)
-                .addClasses(WithinWindowMatcher.class, Ping.class, PingsListener.class, ProgrammaticTimerBean.class);
+        return create(WebArchive.class)
+                .addAsLibraries(
+                    Maven.resolver().loadPomFromFile("pom.xml")
+                        .resolve("com.jayway.awaitility:awaitility")
+                        .withTransitivity().asFile())
+                .addClasses(
+                    WithinWindowMatcher.class, 
+                    Ping.class, 
+                    PingsListener.class, 
+                    ProgrammaticTimerBean.class);
     }
 
     @Test
     public void should_receive_two_pings() {
-        await().untilCall(to(pings.getPings()).size(), equalTo(2));
+        await()
+            .untilCall(
+                to(pings.getPings()).size(), greaterThanOrEqualTo(2));
 
         Ping firstPing = pings.getPings().get(0);
         Ping secondPing = pings.getPings().get(1);
 
         long delay = secondPing.getTime() - firstPing.getTime();
         System.out.println("Actual timeout = " + delay);
+        
         assertThat(delay, is(withinWindow(TIMEOUT, TOLERANCE)));
     }
 }
