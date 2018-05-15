16a738243fef6eb7416e12493e027c43d8f9ea23
arjantijms <arjan.tijms@gmail.com>
2018-05-15 22:08:42 +0200

    Created actual test archive in test for JAX-RPC

--- jaxrpc/jaxrpc-endpoint/src/test/java/org/javaee7/jaxrpc/endpoint/HelloTest.java
+++ jaxrpc/jaxrpc-endpoint/src/test/java/org/javaee7/jaxrpc/endpoint/HelloTest.java
@@ -1,5 +1,9 @@
 package org.javaee7.jaxrpc.endpoint;
 
+import static org.jboss.shrinkwrap.api.ShrinkWrap.create;
+
+import java.io.File;
+
 //import static org.junit.Assert.assertEquals;
 
 import java.net.MalformedURLException;
@@ -8,7 +12,6 @@ import java.net.URL;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +20,27 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class HelloTest {
 
+    private static final String WEBAPP_SRC = "src/main/webapp";
+
     @ArquillianResource
     private URL url;
-   
+
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class).
-            addPackage("org.javaee7.jaxrpc.endpoint");
+        System.out.println("************************************************************");
+
+        WebArchive war =
+            create(WebArchive.class)
+                .addClasses(HelloService.class, HelloServiceImpl.class)
+                .addAsWebInfResource(new File(WEBAPP_SRC + "/WEB-INF", "mapping.xml"))
+                .addAsWebInfResource(new File(WEBAPP_SRC + "/WEB-INF", "web.xml"))
+                .addAsWebInfResource(new File(WEBAPP_SRC + "/WEB-INF", "webservices.xml"))
+                .addAsWebInfResource(new File(WEBAPP_SRC + "/WEB-INF/wsdl", "MyHelloService.wsdl"), "wsdl/MyHelloService.wsdl");
+
+        System.out.println(war.toString(true));
+        System.out.println("************************************************************");
+
+        return war;
     }
 
     @Before
@@ -34,5 +51,5 @@ public class HelloTest {
     public void testHello() throws MalformedURLException {
     }
 
-   
+
 }
