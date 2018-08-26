383a070a5ad06c5bd6e55b3302c60f0961124e10
arjantijms <arjan.tijms@gmail.com>
2018-08-26 20:35:10 +0200

    Added remote EJB sample with Payara/GlassFish concrete impl

---
+++ ejb/remote/roles-allowed/src/main/java/org/javaee7/ejb/remote/remote/Bean.java
@@ -0,0 +1,20 @@
+/** Copyright Payara Services Limited **/
+package org.javaee7.ejb.remote.remote;
+
+import java.io.Serializable;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+
+@Stateless
+public class Bean implements BeanRemote, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    @RolesAllowed("g1")
+    public String method() {
+        return "method";
+    }
+
+}
---
+++ ejb/remote/vendor/README.md
@@ -0,0 +1,12 @@
+# Java EE 7 Samples: EJB - Remote - Vendor #
+
+This module contains vendor specific implementations to obtain the JNDI context from where remote EJB beans can be requested
+from with a username/password credential.
+
+## Implementations ##
+
+ - payara-glassfish - An implementation that works for both Payara and GlassFish 
+
+
+
+
